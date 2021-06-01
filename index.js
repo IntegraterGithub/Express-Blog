c7f4345b5d2bcebb42376ded44014a87ca291240
IntegraterGithub <81035173+IntegraterGithub@users.noreply.github.com>
2021-06-01 20:48:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ mongoose.connect("mongodb+srv://dbUser:dbPassword@cluster0.osuxa.mongodb.net/myB
 	useUnifiedTopology: true
 })
 app.use(express.urlencoded({extended: false}));
-var get_ip = require('ipware')().get_ip;
+
 var articleSchema = new mongoose.Schema({
 	title: String, 
 	description: String, 
 	content: String,
-	date: String,
-	views: Number,
-	viewersIp: Array
+	date: String
 })
 var articleModel = mongoose.model("articles", articleSchema);
 var newPost = (post) => {
@@ -37,12 +35,8 @@ res.render("index", {articles: articles})
 app.get("/article/:title", async (req, res) => {
 const article = await articleModel.findOne({title: req.params.title.split("-").join(" ")}).lean();
 if(!article) return res.send("not found");
-var ip_info = get_ip(req);
-     console.log(ip_info);
-if(article.viewersIp.indexOf(ip_info.clientIp) === -1) {
-	article.views = article.views + 1
-	article.save();
-}
+
+    
 res.render("article", {article: article})
 })
 app.listen(process.env.PORT || 3000, () => { 
