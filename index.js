78a80b1c11755338053aae8427e705ee6cb8a5fd
IntegraterGithub <81035173+IntegraterGithub@users.noreply.github.com>
2021-06-01 20:17:35 +0100

    ip logging
    allows view count!

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://dbUser:dbPassword@cluster0.osuxa.mongodb.net/myBlog", {
 	useNewUrlParser: true, 
 	useUnifiedTopology: true
-
 })
-var get_ip = require('ipware')().get_ip;
-
 app.use(express.urlencoded({extended: false}));
-
+var get_ip = require('ipware')().get_ip;
 var articleSchema = new mongoose.Schema({
 	title: String, 
 	description: String, 
 	content: String,
-	date: String
+	date: String,
+	views: Number,
+	viewersIp: Array
 })
 var articleModel = mongoose.model("articles", articleSchema);
 var newPost = (post) => {
@@ -22,23 +21,31 @@ var Post = new articleModel(post);
 Post.save()
 }
 
-
+app.get("/post", (req, res) => {
+	
+ //const article = await articleModel.findOne({}).lean();
+})
 
 app.set('view engine', "ejs");
 
 app.get('/', async (req, res) => {
- var ip_info = get_ip(req);
-     console.log(ip_info);
-const articles = await articleModel.find({}).sort({date: 1}).lean();
+  
+ const articles = await articleModel.find({}).sort({date: 1}).lean();
 
 res.render("index", {articles: articles})
 })
 app.get("/article/:title", async (req, res) => {
- const article = await articleModel.findOne({title: req.params.title.split("-").join(" ")}).lean();
-if(!article) return res.send("not found")
+const article = await articleModel.findOne({title: req.params.title.split("-").join(" ")}).lean();
+if(!article) return res.send("not found");
+var ip_info = get_ip(req);
+     console.log(ip_info);
+if(article.viewersIp.indexOf(ip_info.ip) === -1) {
+	article.views++
+	article.save();
+}
 res.render("article", {article: article})
 })
-app.listen(process.env.PORT || 8080, () => { 
+app.listen(3000, () => { 
 	console.log(`App listening!`)
 })
 console.log(Date());
